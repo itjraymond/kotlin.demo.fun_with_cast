de12e05f31280124b1c74eca863e60bb1bb08e93
Jocelyn Raymond <it.jraymond@gmail.com>
2020-03-29 09:33:00 -0600

    Adding initial Spring Initializr generated code

---
+++ src/main/kotlin/ca/jent/fun_with_cast/FunWithCastApplication.kt
@@ -0,0 +1,11 @@
+package ca.jent.fun_with_cast
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FunWithCastApplication
+
+fun main(args: Array<String>) {
+	runApplication<FunWithCastApplication>(*args)
+}
---
+++ src/test/kotlin/ca/jent/fun_with_cast/FunWithCastApplicationTests.kt
@@ -0,0 +1,13 @@
+package ca.jent.fun_with_cast
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FunWithCastApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
